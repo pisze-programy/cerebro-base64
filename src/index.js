ca19bfc831f9642390e864fe48550454942214d5
karbz0ne <dev@blaszczyk.tk>
2017-03-16 20:24:10 +0100

    Plugin improvement. After method select, clear methods list. Removed problem with non-existing match error. Just 'base64' keyword will render list.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import Base64 from './base64';
 import Preview from 'Preview';
 
+const keyword = 'base64';
+
 const Plugin = ({ term, display, actions }) => {
   const Methods = {
+    ENCODE: 'encode',
     DECODE: 'decode',
-    ENCODE: 'encode'
   };
 
-  const matcher = (str, expression) => str.match(expression);
-  const termMatch = matcher(term, /^base64\s+(.+)/i);
+  const termMatch = term.match(/^base64\s+(.+)/i);
 
   if (!term.startsWith('base64')) return;
+
+  if (termMatch && termMatch[1].match(/^encode\s+(.+)/i)) {
+    const stringToEncode = termMatch[1].replace(Methods.ENCODE, '');
+    const decoded = new Base64().encode(stringToEncode);
+
+    return display({
+      title: `= ${decoded}`,
+      onSelect: (event) => actions.copyToClipboard(decoded)
+    })
+  }
+
+  if (termMatch && termMatch[1].match(/^decode\s+(.+)/i)) {
+    const stringToDecode = termMatch[1].replace(Methods.DECODE, '');
+    const encoded = new Base64().decode(stringToDecode);
+
+    return display({
+      title: `= ${encoded}`,
+      onSelect: (event) => actions.copyToClipboard(encoded)
+    })
+  }
 
   Object.keys(Methods).map((method, index) => {
     display({
@@ -25,31 +46,11 @@ const Plugin = ({ term, display, actions }) => {
       }
     });
   });
-
-  if (matcher(termMatch[1], /^encode\s+(.+)/i)) {
-    const stringToEncode = termMatch[1].replace(Methods.ENCODE, '');
-    const decoded = new Base64().encode(stringToEncode);
-
-    return display({
-      title: `= ${decoded}`,
-      onSelect: (event) => actions.copyToClipboard(decoded)
-    })
-  }
-
-  if (matcher(termMatch[1], /^decode\s+(.+)/i)) {
-    const stringToDecode = termMatch[1].replace(Methods.DECODE, '');
-    const encoded = new Base64().decode(stringToDecode);
-
-    return display({
-      title: `= ${encoded}`,
-      onSelect: (event) => actions.copyToClipboard(encoded)
-    })
-  }
 };
 
 module.exports = {
   fn: Plugin,
-  keyword: 'base64'
+  keyword
 };
 
 export default Plugin;
